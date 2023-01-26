0513afb4cf6b0c350d46c1d690d8931246f8c767
Seth Lang <sethwlang@protonmail.com>
2023-01-25 20:22:39 -0500

    coaching page changes WIP

--- src/views/Unstuck.vue
+++ src/views/Unstuck.vue
@@ -6,65 +6,219 @@
         <picture>
           <source type="image/webp" srcset="../assets/images/coffeeshop.webp">
           <source type="image/jpeg" srcset="../assets/images/coffeeshop.jpg">
-            <img   src="../assets/images/coffeeshop.jpg" />
+          <img src="../assets/images/coffeeshop.jpg" />
         </picture>
       </div>
       <div class="flex vertical center content">
         <div class="text-content">
           <h1 class="handwriting  ">Get Unstuck Coaching Program</h1>
-          <p  >
-          If you’re like a lot of writers, finding (or making) time to write without external accountability feels
-          impossible—it’s too easy to put off the writing until a someday that never comes. However, the American
-          Society of Training and Development found that accountability appointments increase your odds of success
-          by <span class="italic">up to 95%!</span>
-          </p>
+          <p>Do you feel like it’s impossible to find time to write, or to build sustainable writing habits that you
+            actually
+            enjoy?</p>
 
-          <p  >That’s why I created the Accountability Program.</p>
+          <p>Are you ready to learn how to write consistently, instead of waiting (and waiting…and waiting) for
+            inspiration to
+            strike?</p>
 
-          <p  >
-          The Accountability Program consists of 6 bi-weekly meetings across 3 months where we:</p> 
-          <ul class="list-disc ml-5    ">
-            <li>Set realistic, achievable goals</li>
-            <li>Build research-driven writing habits</li>
-            <li>Analyze and overcome challenges, and</li>
-            <li>Review your progress.</li>
+          <h4>Then the Get Unstuck Coaching Program is for you.</h4>
+
+          <p>You have an amazing idea for a book. It’s a story you know you want to tell—a story you know will make a
+            difference for your readers.</p>
+
+          <p>Maybe you’ve even started trying to write it. You bought the cute notebook and the perfect pens. You set
+            some
+            writing goals. You envisioned the finished book on the shelf at your favorite local bookstore. </p>
+
+          <p>Then you tried to put your writing plan into action…and it all fell apart. </p>
+
+          <ul class="list-disc ml-5">
+            <li>Your life is busy—there’s always something that seems like it should take priority over your writing (no
+              matter how passionate you are about it).</li>
+            <li>There are constant distractions—social media is right there, your phone notifications are going off, and
+              your
+              kid wants a snack. </li>
+            <li>Not only that, but the blank page is intimidating—the path from “Once upon a time” to “The end” is
+              winding and
+              murky. You wonder: Are you really up for the challenge?</li>
           </ul>
-          <p  >You get regular, one-on-one access to a highly trained writing educator—that’s me!—to keep you on track and celebrate each milestone you reach.</p>
+
+          <p>I know so many writers who have an incredible book idea and a draft they can’t ever seem to finish. </p>
+
+          <p>hey can’t figure out how to write consistently, so that cute notebook sits on a shelf, gathering dust. They
+            fall
+            into a boom and bust cycle of writing frantically, burning out, and then not writing again for months (or
+            years),
+            feeling guilty all the while. The book doesn’t get written. </p>
+
+          <p>You have the idea—so why is it so hard to sit down and write it?</p>
+
         </div>
 
       </div>
     </div>
-   
-      <div class="text-content paddington  ">
-        <p>
-        You also get worksheets for each meeting, which means you’ll finish this program with progress on your project
-        <span class="italic">and</span> improved insights into your own writing process: what works, what doesn’t, and how to keep yourself on
-        track.
-        </p>
 
-        <p>
-        You’ve waited long enough to finish your book. It’s time to take the next step.</p>
-        <p>I have a limited number of Accountability Program spots open for the July-September Session! <a href="https://calendly.com/the-writing-desk/free-consultation"   target="_blank">Contact me</a> to learn more
-        today about the program and set up an initial free consultation!
-        </p>
+    <div class="text-content paddington  ">
+      <p>You want your writing life to feel easy. Joyful. Spacious. Consistent. </p>
 
-         <a href="https://calendly.com/the-writing-desk/free-consultation" class="primary-button" ><span>Contact Bailey</span></a>
-      </div>
-    
-    <p class="quote"><i>“Almost immediately, I was excited about my writing again. It felt different approaching my writing sessions
-      knowing that I am not the only one engaged in them, that someone else also cares and is there for support, should
-      I need it. I started a daily writing streak that is now 40 days and going strong, and I went from 31K words to
-      over 63K words.”<strong>—Milena Velez, Chapin, SC. YA and Children’s Author.</strong></i></p>
-    
+      <p>You want to be able to tell your story and get your book out into the world. </p>
+
+      <p>You want your writing process to feel less like pulling teeth and more like a dance with your imagination. </p>
+
+      <p>Maybe you’ve already spent hours researching writer’s block, and you’ve tried to implement the strategies:
+        “Butt in chair, fingers on keyboard” is a common one. Just white-knuckle it! If you want to write badly enough,
+        you’ll do it!</p>
+
+      <p>That approach never works for long, and here’s why: willpower is a finite resource. You can’t simply make
+        yourself write, as you surely know if you’ve tried! </p>
+
+      <p>Or maybe you’ve tried bullying yourself into writing, bringing out your inner drill sergeant to yell at you
+        until you sit down and put words on the page. Bullying yourself into writing will fail, too. </p>
+
+      <p>So will waiting for your schedule to line up perfectly: something always gets in the way. </p>
+
+      <p>Here’s the thing: it’s not your fault that none of these strategies have worked. They were never going to work,
+        because none of them focus on what it really takes to build a consistent writing practice.</p>
+      <h4>That’s why I created Get Unstuck.</h4>
+
+      <p>Get Unstuck is a 5-month coaching program focused on the strategies that will help you write consistently. </p>
+
+      <p>During your 10 one-on-one coaching sessions, you’ll learn how to build, experiment with, customize, implement,
+        and sustain effective writing habits. </p>
+      <p>By the time the program is done, you’ll have the tools you need to build a consistent writing practice that
+        aligns with your energy, your life, and your schedule. </p>
+
+
+    </div>
+
+    <p class="quote"><i>“Almost immediately, I was excited about my writing again. It felt different approaching my
+        writing sessions
+        knowing that I am not the only one engaged in them, that someone else also cares and is there for support,
+        should
+        I need it. I started a daily writing streak that is now 40 days and going strong, and I went from 31K words to
+        over 63K words.”<strong>—Milena Velez, Chapin, SC. YA and Children’s Author.</strong></i></p>
+    <div class="text-content paddington  ">
+      <p>Imagine your writing life made you feel as infinitely creative as you are—if writing was something you did
+        regularly, joyfully, and easily. </p>
+
+      <p>What would that mean for the story you want to tell? What would it mean for the story after that?</p>
+
+      <p>You already know you have the idea and the creative writing chops to bring it to life. What you don’t yet have
+        is a flexible, resourceful set of writing habits that work for your life, your schedule, and your needs. </p>
+
+      <p>But you will. </p>
+
+      <p>The Get Unstuck program is going to teach you how to completely reimagine your writing process through the lens
+        of research-based writing habits. </p>
+
+      <p>Get ready to write consistently. </p>
+      <p>Get ready to kick writer’s block to the curb. </p>
+      <p>Get ready to tell your story. </p>
+
+      <h4>Get ready to Get Unstuck.</h4>
+
+      <p>Get Unstuck provides 5 months of one-on-one coaching with a highly trained writing educator (that’s me!).
+        During this process, I’m going to be your guide as you examine your current writing practices, experiment with
+        research-based writing habits, and focus on setting achievable writing goals. </p>
+
+      <p>Here’s how it works:</p>
+
+      <p>Each month, you’ll get 2 one-on-one coaching sessions with me. </p>
+
+      <p>We’ll spend an hour dissecting your approach to writing, reviewing the logistical and mental roadblocks that
+        get between you and the page, and coming up with customized approaches to writing that help you form and
+        maintain writing habits. </p>
+
+      <p>You’ll learn:</p>
+      <p>New writing habits that are based on cutting-edge research: We’ll work together to create and customize a whole
+        new writing life—one that helps you write consistently.
+        How to assess your existing writing process: Figure out what’s been working (and what hasn’t), and uncover how
+        your self-talk affects your writing.
+        Strategies for finding time to write: You don’t need as much time as you might think, and you probably have more
+        time available than you believe you do!
+        Methods for setting goals in a way that works for you: Start setting achievable goals that inspire and support
+        you, rather than create additional pressure.
+        Ways you can analyze and adjust your new habits: Life happens! Once you have your new habits, you’ll also be
+        empowered to tweak them to fit any changes to your schedule or goals.</p>
+
+      <p>Bonuses</p>
+      <p>In addition to one-on-one coaching, you’ll also get a set of invaluable resources that you can return to again
+        and again. You’ll receive:
+        A Get Unstuck workbook. You and I will both contribute to the workbook throughout the program. At the end of Get
+        Unstuck, you’ll have a detailed record of your reflections, my suggestions, and your experiments with new
+        habits.
+        Progress trackers. As you begin setting new goals, you’ll get a set of progress trackers that align with the
+        goal type and help you see your growth across time.
+        Access to me between sessions. While we’ll meet live bi-weekly, writing questions can pop up at any time. You
+        can contact me with questions between sessions, and I’ll provide ongoing guidance throughout the program. </p>
+
+      <p>Get Unstuck costs $2,700, which you can pay in full or through six monthly payments of $450. </p>
+    </div>
     <div class="materials-pack">
-      <h2 class="text-center handwriting">Accountability Materials Pack</h2>
-      <p class="">If the full accountability program isn’t a good fit right now, you can still access many of the benefits!</p><p>The
+      <h2 class="text-center handwriting">Get Unstuck Coaching Program</h2>
+      <p class="">If the full accountability program isn’t a good fit right now, you can still access many of the
+        benefits!</p>
+      <p>The
         Accountability Materials Pack includes worksheets and resources that you can use to create your own writing
         goals and track your progress.</p>
 
-<script src="https://gumroad.com/js/gumroad.js" type="application/javascript" defer></script>
-<a class="primary-button" href="https://thewritingdesk.gumroad.com/l/szrdl" data-gumroad-single-product="true"><span>Get the Accountability Materials Pack</span></a>
-      
+      <script src="https://gumroad.com/js/gumroad.js" type="application/javascript" defer></script>
+      <a class="primary-button" href="https://thewritingdesk.gumroad.com/l/szrdl"
+        data-gumroad-single-product="true"><span>Get the Get Unstuck Coaching Program</span></a>
+
     </div>
+    <div class="paddington  bg-primary-light">
+    <div class="text-content">
+
+      <p></p>Are you ready to get unstuck? Schedule an initial meeting with me. During this free 30-minute call, we’ll set up your coaching schedule, decide on a payment plan, and go over your questions. You’ll also receive all the materials you need to get started. </p>
+
+      <p>Still have questions? I get it! Schedule a free call with me to talk about the program in more detail. </p>
+
+        <p>If you’re thinking that “real” writers work solo, that writing is supposed to be a miserable slog, and that you can figure it out on your own, I want you to stop and take a breath. </p>
+
+          <p>Consider, just for a moment, that the myth of the lone genius writer might be just that: a myth. Imagine if writing didn’t have to be such a struggle. Think about what it might be like to not have to walk this path by yourself. </p>
+
+            <p>You’ve already tried—maybe repeatedly—to create a sustainable writing practice. You’ve dealt with the frustration, the guilt, and the shame of knowing you have a story to tell, but not knowing how to get yourself to tell it. </p>
+
+              <p>It’s time to stop trying to do it all alone. </p>
+
+                <p>What if you had a partner in this process? Not someone who will yell at you or echo your inner critic, but a genuine guide: someone who’s just a little further along in their journey and who’s willing to help. </p>
+
+                  <p>All you have to do is ask.</p>
+
+    </div>
+    </div>
+    <div class="text-content paddington">
+      <p></p>I know asking for help is scary. Get Unstuck is designed to provide that help in the least scary way possible. </p>
+
+      <p>Through one-on-one meetings, reflective journal questions, co-writing sessions, and the creation of a completely customized set of writing habits, we’ll work together to plant the seeds of a writing practice that helps you produce more than you ever thought possible. </p>
+
+        <p>Your book deserves to be written. Are you ready to write it?</p>
+
+          <p>Schedule an initial meeting with me. During this free 30-minute call, we’ll set up your coaching schedule, decide on a payment plan, and go over your questions. You’ll also receive all the materials you need to get started. </p>
+
+            <p>Get Unstuck costs $2,700, which you can pay in full or through six monthly payments of $450. </p>
+
+<h4>Get Unstuck is not for you if you’re looking for…</h4>
+
+<p></p>A creative writing workshop. Get Unstuck is focused on building writing habits, not writing in a specific genre or style. </p>
+<p></p>An easy, one-size-fits-all solution. That’s not how it works (sorry!). </p>
+<p></p>A coach who will yell at you if you miss a goal or “fail” to write—I’ll help hold you accountable, but I’m never going to be mean about it. </p>
+<p></p>A “set it and forget it” approach to writing consistently. While I wish I could give everyone The One Right Way To Write, Now and Forever, it doesn’t exist. Your life will change, and your writing habits need to change along with it. What I can give you are the tools you need to approach your writing with flexibility and sustainability in mind. </p>
+
+<p></p>Still wondering whether Get Unstuck is the right fit for you? Sign up for a free call with me and let’s chat—no strings attached and no high-pressure sales pitch, I promise! </p>
+
+<p></p>Get Unstuck is for writers who want to write consistently—writers who are ready to make a change in their writing habits, and who are willing to experiment. </p>
+
+<p>Get Unstuck is for you if you’re ready to make a change. This program is for you if you’re willing to put in the effort and experiment. Get Unstuck is for you if you’re ready to write consistently. </p>
+
+  <p>I know you can have the writing life you want, and I know better writing habits can help you make real, sustainable progress. </p>
+
+    <p>I also know what it’s like to feel stuck—I’m here to help explode those feelings and pave the way to a writing life you truly enjoy. Are you ready?</p>
+
+
+<p class="text-xs">Disclaimer: There is no guarantee that the Get Unstuck coaching program will produce any specific level of success in your writing practices or provide a specific outcome. Numerous factors contribute to determining your experience and results and, as with any class, training experience, or coaching relationship, your results are determined by how you apply the material. </p>
+
+    </div>
+    
   </div>
 </template>
